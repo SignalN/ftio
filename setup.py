c04031febf4a0fff8f334d205ec6f870dea4c7aa
bittlingmayer <adam+code@signaln.com>
2017-08-28 04:10:33 -0700

    Add packaging boilerplate and one bit of functionality - the python implementation of the preprocessing for the Wiki pre-trained models released for a few hundred languages

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ftio',
+  packages = ['ftio'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Python tools for working with fastText, an open-source library from the Facebook AI Research lab',
+  author = 'Signal N',
+  author_email = 'ftio@signaln.com',
+  url = 'https://github.com/signaln/ftio',
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['fastText', 'word2vec', 'word embeddings', 'natural language processing', 'text'],
+  classifiers = [],
+)
