6be4cffd4086bdf869c3a0110449d42d951af4cb
bittlingmayer <adam+code@signaln.com>
2017-08-28 07:01:21 -0700

    Fix regex list

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name = 'ftio',
   packages = ['ftio', 'ftio.wiki'], # this must be the same as the name above
-  version = '0.6',
+  version = '0.7',
   description = 'Python tools for working with fastText, an open-source library from the Facebook AI Research lab',
   author = 'Signal N',
   author_email = 'ftio@signaln.com',
   url = 'https://github.com/signaln/ftio',
-  download_url = 'https://github.com/signaln/ftio/archive/0.6.tar.gz',
+  download_url = 'https://github.com/signaln/ftio/archive/0.7.tar.gz',
   keywords = ['fastText', 'word2vec', 'word embeddings', 'natural language processing', 'text'],
   classifiers = [],
 )
