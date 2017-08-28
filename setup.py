8ba3c8da7924896e2172e57e0815d4d51081ee04
bittlingmayer <adam+code@signaln.com>
2017-08-28 05:52:41 -0700

    Fix missing __init__.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name = 'ftio',
   packages = ['ftio'], # this must be the same as the name above
-  version = '0.3',
+  version = '0.5',
   description = 'Python tools for working with fastText, an open-source library from the Facebook AI Research lab',
   author = 'Signal N',
   author_email = 'ftio@signaln.com',
   url = 'https://github.com/signaln/ftio',
-  download_url = 'https://github.com/signaln/ftio/archive/0.3.tar.gz',
+  download_url = 'https://github.com/signaln/ftio/archive/0.5.tar.gz',
   keywords = ['fastText', 'word2vec', 'word embeddings', 'natural language processing', 'text'],
   classifiers = [],
 )
